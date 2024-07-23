23356c75f7e2379f2d1b074013f423a033b001fb
Kevin Fang <kevinfang1098@gmail.com>
2024-07-22 21:37:00 -0700

    tried to fix stuff oh well

--- server.py
+++ server.py
@@ -1,13 +1,25 @@
 import http.server
 import socketserver
+import os
 
 PORT = 8000
 
-Handler = http.server.SimpleHTTPRequestHandler
-Handler.extensions_map.update({
-    ".js": "application/javascript",
-})
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Cache-Control', 'no-cache, no-store, must-revalidate')
+        self.send_header('Pragma', 'no-cache')
+        self.send_header('Expires', '0')
+        super().end_headers()
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Serving at http://localhost:{PORT}")
-    httpd.serve_forever()
+def run_server():
+    with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+        print(f"Serving at http://localhost:{PORT}")
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print("\nShutting down the server...")
+            httpd.shutdown()
+            httpd.server_close()
+
+if __name__ == "__main__":
+    run_server()
